53758f165a5778fcd26c7c438d4e87bf7efda472
a18matna <49142171+a18matna@users.noreply.github.com>
2021-04-06 21:18:44 +0200

    sökning implementerad
    sökning efter former implementerad, sökning för bokningar startad

--- Applikation.php
+++ Applikation.php
@@ -23,13 +23,13 @@ $conn = $database->getConnection();
         <input type='submit' value='hämta alla bokningar'>
     </form>
     <h1> Sök efter Form</h1>
-    <form action='formsearch-service.php' method='get'>
-        <input type='search' Placeholder='sök efter form' value=''>
+    <form action='formsearch-service.php' method='post'>
+        <input type='search' Placeholder='sök efter form' value='' name='user_search'>
         <input type='submit' value='sök'>
     </form>
     <h1> Sök efter Bokning</h1>
-    <form action='bokningsearch-service.php' method='get'>
-        <input type='search' Placeholder='sök efter bokning' value=''>
+    <form action='bokningsearch-service.php' method='post'>
+        <input type='search' Placeholder='sök efter bokning' value='' name='user_boksearch'>
         <input type='submit' value='sök'>
     </form>
 </body>
--- bokningsearch-service.php
+++ bokningsearch-service.php
@@ -4,6 +4,10 @@ include_once 'database.php';
 //Create database connection
 $database = new Database();
 $conn = $database->getConnection();
+$sql = "SELECT * FROM forms where '".$_POST['user_boksearch']."' in (idForms, typeForms, colorForms, sizeForms) ";
+$result = $conn->query($sql);
+$row = $result ->fetchall(PDO::FETCH_ASSOC);
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,6 +17,28 @@ $conn = $database->getConnection();
     <title>Document</title>
 </head>
 <body>
-    
+<div id='searchformdiv'>
+        <table>
+            <tr>
+            <th> Sök resultat </th>
+            </tr>
+            <tr>
+            <th> ID </th><th> TYP </th> <th> FÄRG </th> <th> STORLEK </th>
+            </tr>
+            <?php 
+        
+            foreach ($row as $data){
+            ?>
+            <tr>
+                <td><?= $data['idForms']?></td>
+                <td><?= $data['typeForms']?></td>
+                <td><?= $data['colorForms']?></td>
+                <td><?= $data['sizeForms']?></td>
+            </tr>
+            <?php
+            }
+            ?>
+        </table>
+    </div>
 </body>
 </html>
